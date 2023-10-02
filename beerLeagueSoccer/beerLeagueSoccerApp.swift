7e48078e660c9063314e02739e2d819ad5997897
kekons4 <kekons4@gmail.com>
2023-10-02 10:24:42 -0400

    Initial Commit

---
+++ beerLeagueSoccer/beerLeagueSoccerApp.swift
@@ -0,0 +1,17 @@
+//
+//  beerLeagueSoccerApp.swift
+//  beerLeagueSoccer
+//
+//  Created by Keon Pourboghrat on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct beerLeagueSoccerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
